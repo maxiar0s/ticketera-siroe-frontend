5df1b9c691901715582c91f326af3b125e200865
Maximiliano Aros <maxi.aros.osorio@gmail.com>
2025-03-14 15:40:52 -0300

    firebase deploy

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -3,50 +3,47 @@ import { Injectable } from '@angular/core';
 import { catchError, map, Observable, of, throwError } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
-  // private url = "https://app-soporte-siroe.vercel.app";
-  private url = "http://localhost:3000";
+  private url = 'https://app-soporte-siroe.vercel.app';
+  // private url = "http://localhost:3000";
 
-  constructor(
-    private http: HttpClient,
-  ) {}
+  constructor(private http: HttpClient) {}
 
-  login(data: any):Observable<any> {
+  login(data: any): Observable<any> {
     const endpoint = 'auth/login';
-    const url = `${this.url}/${endpoint}`
-    return this.http.post<any>(url, data)
-      .pipe(
-        catchError((error) => {
-          console.error(error);
-          return of(null);
-        })
-      );
+    const url = `${this.url}/${endpoint}`;
+    return this.http.post<any>(url, data).pipe(
+      catchError((error) => {
+        console.error(error);
+        return of(null);
+      })
+    );
   }
 
   // POST
-  createClient(data: any):Observable<any> {
+  createClient(data: any): Observable<any> {
     const endpoint = 'ingresar-cliente';
     return this.postInformation(data, endpoint);
   }
 
-  createModifyBranch(data: any):Observable<any> {
+  createModifyBranch(data: any): Observable<any> {
     const endpoint = 'ingresar-sucursal';
     return this.postInformation(data, endpoint);
   }
 
-  createModifyUser(data: any):Observable<any> {
+  createModifyUser(data: any): Observable<any> {
     const endpoint = 'crear-modificar-cuenta';
     return this.postInformation(data, endpoint);
   }
 
-    // Equiptment
-  createEquiptment(data: any):Observable<any> {
+  // Equiptment
+  createEquiptment(data: any): Observable<any> {
     const endpoint = 'ingresar-equipo';
     return this.postInformation(data, endpoint);
   }
-  modifyEquiptment(data:any):Observable<any> {
+  modifyEquiptment(data: any): Observable<any> {
     let formValues: { [key: string]: any } = {};
     data.forEach((value: any, key: any) => {
       formValues[key] = value;
@@ -55,73 +52,73 @@ export class ApiService {
     const endpoint = 'modificar-equipo/' + id;
     return this.postInformation(data, endpoint);
   }
-  createComment(data:any):Observable<any> {
+  createComment(data: any): Observable<any> {
     const { equipoId: id } = data;
     const endpoint = 'ingresar-observacion/' + id;
     return this.postInformation(data, endpoint);
   }
 
   // GET
-  deleteUser(id: number):Observable<any> {
+  deleteUser(id: number): Observable<any> {
     const endpoint = `eliminar-cuenta/${id}`;
     return this.getInformation(endpoint);
   }
 
-  deleteBranch(id: string):Observable<any> {
+  deleteBranch(id: string): Observable<any> {
     const endpoint = `eliminar-sucursal/${id}`;
     return this.getInformation(endpoint);
   }
 
-    // Casas Matricez
-  clients(pagina: number):Observable<any> {
+  // Casas Matricez
+  clients(pagina: number): Observable<any> {
     const endpoint = `clientes?pagina=${pagina}`;
     return this.getInformation(endpoint);
   }
 
-  client(id: string, pagina: number, option: string):Observable<any> {
-    let endpoint = 'cliente/'+id+`?pagina=${pagina}`;
-    if(option) {
-      endpoint = 'cliente/'+id+`?pagina=${pagina}&option=${option}`;
+  client(id: string, pagina: number, option: string): Observable<any> {
+    let endpoint = 'cliente/' + id + `?pagina=${pagina}`;
+    if (option) {
+      endpoint = 'cliente/' + id + `?pagina=${pagina}&option=${option}`;
     }
     return this.getInformation(endpoint);
   }
 
-    // Sucursal
-  sucursal(id: string, pagina: number, option: string):Observable<any> {
-    let endpoint = `sucursal/`+id+`?pagina=${pagina}`;
-    if(option) {
-      endpoint = `sucursal/`+id+`?pagina=${pagina}&option=${option}`;
+  // Sucursal
+  sucursal(id: string, pagina: number, option: string): Observable<any> {
+    let endpoint = `sucursal/` + id + `?pagina=${pagina}`;
+    if (option) {
+      endpoint = `sucursal/` + id + `?pagina=${pagina}&option=${option}`;
     }
     return this.getInformation(endpoint);
   }
 
-    // Crear o modificar usuario
-  users(pagina: number, option: string):Observable<any> {
-    let endpoint = `usuarios`+`?pagina=${pagina}`;
-    if(option) {
-      endpoint = endpoint+`&option=${option}`;
+  // Crear o modificar usuario
+  users(pagina: number, option: string): Observable<any> {
+    let endpoint = `usuarios` + `?pagina=${pagina}`;
+    if (option) {
+      endpoint = endpoint + `&option=${option}`;
     }
     return this.getInformation(endpoint);
   }
 
-  typeEquipments():Observable<any> {
+  typeEquipments(): Observable<any> {
     const endpoint = 'tipos-equipos';
     return this.getInformation(endpoint);
   }
 
-  formEquipment(tipoEquipoId: string):Observable<any> {
+  formEquipment(tipoEquipoId: string): Observable<any> {
     const endpoint = `obtener-formulario/` + tipoEquipoId;
     return this.getInformation(endpoint);
   }
 
-  equipmentsByCasaMatriz(id: string):Observable<any> {
+  equipmentsByCasaMatriz(id: string): Observable<any> {
     const endpoint = `cliente/` + id + `/equipos`;
     return this.getInformation(endpoint);
   }
 
-    // Obtener equipo por ID
-  equiptment(id: number):Observable<any> {
-    const endpoint = `equipo/`+ id;
+  // Obtener equipo por ID
+  equiptment(id: number): Observable<any> {
+    const endpoint = `equipo/` + id;
     return this.getInformation(endpoint);
   }
 
@@ -138,29 +135,32 @@ export class ApiService {
 
   postInformation(data: any, endpoint: string) {
     const url = `${this.url}/${endpoint}`;
-    return this.http.post<any>(url, data)
-      .pipe(
-        catchError((error) => {
-          console.error(error);
-          return of(null);
-        })
-      );
+    return this.http.post<any>(url, data).pipe(
+      catchError((error) => {
+        console.error(error);
+        return of(null);
+      })
+    );
   }
 
   // Consumo de imagenes GCS (Google Cloud Storage)
-  signedUrl(fileName: string):Observable<any> {
+  signedUrl(fileName: string): Observable<any> {
     const apiUrl = `${this.url}/api/generar-url/${fileName}`;
-    return this.http.get<{ signedUrl: string }>(apiUrl).pipe(
-      map((response) => response.signedUrl)
-    );
+    return this.http
+      .get<{ signedUrl: string }>(apiUrl)
+      .pipe(map((response) => response.signedUrl));
   }
 
   // Verificar si correo electronico existe en la cuenta
   verificarCorreoExistente(correo: string): Observable<boolean> {
-    return this.http.get<{ isTaken: boolean }>(`${this.url}/verificar-correo?correo=${correo}`).pipe(
-      map(response => {
-        return response.isTaken;
-      }),
-    );
+    return this.http
+      .get<{ isTaken: boolean }>(
+        `${this.url}/verificar-correo?correo=${correo}`
+      )
+      .pipe(
+        map((response) => {
+          return response.isTaken;
+        })
+      );
   }
 }
